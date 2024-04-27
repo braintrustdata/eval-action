4c5d3e0aa3f282b9336e23d87dc0421eade9eb9d
Ankur Goyal <ankrgyl@gmail.com>
2024-04-27 16:39:15 -0700

    More

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import * as core from "@actions/core";
 import z from "zod";
 
+import * as util from "util";
+import * as path from "path";
+import { exec as execSync } from "child_process";
+
+const exec = util.promisify(execSync);
+
 const params = z.strictObject({
   root: z.string(),
   paths: z.string(),
@@ -11,23 +17,30 @@ const params = z.strictObject({
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-export async function run(): Promise<void> {
-  try {
-    const argsP = params.safeParse({
-      root: core.getInput("root"),
-      paths: core.getInput("paths"),
-      runtime: core.getInput("runtime")
-    });
-    if (!argsP.success) {
-      throw new Error(
-        "Invalid arguments: " +
-          argsP.error.errors.map(e => e.message).join("\n")
-      );
-    }
-
-    core.debug("Hello, world!");
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message);
+async function main(): Promise<void> {
+  const args = params.safeParse({
+    root: core.getInput("root"),
+    paths: core.getInput("paths"),
+    runtime: core.getInput("runtime")
+  });
+  if (!args.success) {
+    throw new Error(
+      "Invalid arguments: " + args.error.errors.map(e => e.message).join("\n")
+    );
   }
+
+  const { root, paths, runtime } = args.data;
+
+  // Change working directory
+  process.chdir(path.resolve(root));
+
+  // Run the command
+  const command = `npx braintrust eval ${paths} --root ${root} --runtime ${runtime}`;
+  await exec(command);
+}
+
+export async function run(): Promise<void> {
+  return main().catch(error => {
+    core.setFailed(error.message);
+  });
 }
