0a5ad311ada748abe1bfd97f3e2ee39cd0f8b3a0
Ankur Goyal <ankrgyl@gmail.com>
2024-04-28 12:43:00 -0700

    Add a bogus eval

---
+++ test-eval/fail.eval.ts
@@ -0,0 +1,11 @@
+import { traced, Eval } from "braintrust";
+
+Eval("My Fail", {
+  data: () => [
+    { input: "Which country has the highest population?", expected: "China" },
+  ],
+  task: async (input, { span }) => {
+    throw new Error("syntax error!");
+  },
+  scores: [exactMatch],
+});
